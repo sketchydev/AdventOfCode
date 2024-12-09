b2f0010ac24a75719b90f83671956479ae02f360
Shane Booth <shane.booth@slimmingworld.co.uk>
2024-12-09 09:03:04 +0000

    day 7, part 2

--- AOC2024/Day7.cs
+++ AOC2024/Day7.cs
@@ -15,7 +15,8 @@
 
             }
 
-            long answer = 0; 
+            long answer1 = 0;
+            long answer2 = 0;
 
             //part 1
 
@@ -25,19 +26,25 @@
 
                 long[] restItems = problem.Value[1..];
 
-                if (Iterate(problem.Value[0], restItems, problem.Key))
+                if (IteratePart1(problem.Value[0], restItems, problem.Key))
                 {
-                    answer += problem.Key;
+                    answer1 += problem.Key;
+                }
+
+                if (IteratePart2(problem.Value[0], restItems, problem.Key))
+                {
+                    answer2 += problem.Key;
                 }
 
             }
 
-            Console.WriteLine("Part 1: " + answer);
+            Console.WriteLine("Part 1: " + answer1);
+            Console.WriteLine("Part 2: " + answer2);
 
 
         }
 
-        public static bool Iterate(long left, long[] right, long target)
+        public static bool IteratePart1(long left, long[] right, long target)
         {
             var calculation_one = left + right[0];
             var calculation_two = left * right[0];
@@ -46,8 +53,8 @@
             {
                 var restItems = right[1..];
 
-                var result_one = Iterate(calculation_one, restItems, target);
-                var result_two = Iterate(calculation_two, restItems, target);
+                var result_one = IteratePart1(calculation_one, restItems, target);
+                var result_two = IteratePart1(calculation_two, restItems, target);
                 
                 if (result_one || result_two)
                 {
@@ -62,6 +69,45 @@
             else 
             {
                 if (calculation_one == target || calculation_two == target)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+
+        public static bool IteratePart2(long left, long[] right, long target)
+        {
+            var calculation_one = left + right[0];
+            var calculation_two = left * right[0];
+            var calculation_three = long.Parse(left.ToString() + right[0].ToString());
+            
+
+            if (right.Length > 1) //keep going
+            {
+                var restItems = right[1..];
+
+                var result_one = IteratePart2(calculation_one, restItems, target);
+                var result_two = IteratePart2(calculation_two, restItems, target);
+                var result_three = IteratePart2(calculation_three, restItems, target);
+
+                if (result_one || result_two || result_three)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            else
+            {
+                if (calculation_one == target || calculation_two == target || calculation_three == target)
                 {
                     return true;
                 }
